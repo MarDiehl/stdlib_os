668b9fcb437b206568a88c3b70cb8555b9f0b47a
arjenmarkus <arjen.markus895@gmail.com>
2020-07-31 21:50:20 +0200

    Integrate the changes for Windows
    Integrate the changes required for Windows and MInGW-w64/MSYS2 into the existing code. The original code works without changes for Cygwin.
    The work is not yet finished, as via CMake I get a bunch of warnings about non-standard names

--- src/internal.c
+++ src/internal.c
@@ -1,9 +1,21 @@
+/* On Windows we need to distinguish between Cygwin (very like Linux),
+   MinGW-w64/MSYS2 (to use the full name) and plain Windows
+
+   The macro _WIN32 is defined on plain Windows and MinGW, not on Cygwin
+*/
+
 #include <string.h>
+#ifndef _WIN32
 #include <pwd.h>
 #include <unistd.h>
+#else
+#include <stdio.h>
+#include <stdlib.h>
+#endif
 #include <limits.h>
 
 /* https://stackoverflow.com/questions/8953424 */
+#ifndef _WIN32
 void getuser_c(char user[], int *stat){
   struct passwd *pw = getpwuid(geteuid());
   if (pw){
@@ -29,3 +41,30 @@ void gethome_c(char home[], int *stat){
 int path_max_c(){
   return PATH_MAX;
 }
+#else
+
+/* On plain WIndows and MinGW use environment variables */
+#if defined(__MINGW32__) || defined(__MINGW64__)
+#define USER "USER"
+#define HOME "HOME"
+#else
+#define USER "USERNAME"
+#define HOME "USERPROFILE"
+#endif
+
+void getuser_c(char user[], int *stat){
+  strcpy( user, getenv(USER) );
+  *stat = (strlen(user) == 0);
+}
+
+void gethome_c(char home[], int *stat){
+  strcpy( home, getenv(HOME) );
+  *stat = (strlen(home) == 0);
+}
+
+int path_max_c(){
+  return _MAX_PATH;
+}
+#endif
+
+
--- src/os.c
+++ src/os.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#ifndef _WIN32
 #include <unistd.h>
+#else
+#include <direct.h>
+#if !defined(__MINGW32__) && !defined(__MINGW64__)
+#define PATH_MAX _MAX_PATH
+#endif
+#define _getcwd getcwd
+#define _mkdir   mkdir
+#define _rmdir   rmdir
+#define _unlink  unlink
+#endif
 
 int chdir_c(const char *path){
   return chdir(path);
@@ -20,9 +32,15 @@ void getcwd_c(char cwd[], int *stat){
   }
 }
 
+#ifndef _WIN32
 int mkdir_c(const char *path, int *mode){
   return mkdir(path,(mode_t)*mode);
 }
+#else
+int mkdir_c(const char *path, int *mode){
+  return _mkdir(path);
+}
+#endif
 
 int rename_c(const char *src, const char *dst){
   return rename(src,dst);
@@ -32,9 +50,15 @@ int rmdir_c(const char *path){
   return rmdir(path);
 }
 
+#ifndef _WIN32
 int symlink_c(const char *src, const char *dst){
   return symlink(src,dst);
 }
+#else
+int symlink_c(const char *src, const char *dst){
+  return 1;
+}
+#endif
 
 int unlink_c(const char *path){
   return unlink(path);
