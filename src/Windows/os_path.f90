24eb66666bbb2d514b813f909ee24571a05a5926
Martin Diehl <mail@martin-diehl.net>
2020-10-17 23:14:34 +0200

    separate OS-specific code

--- src/Windows/os_path.f90
+++ src/Windows/os_path.f90
@@ -3,18 +3,78 @@ submodule(os_path) os_path
   contains
 
   pure function isabs(path)
-  
+
     logical                      :: isabs
     character(len=*), intent(in) :: path
-  
+
     if(len(path) == 0) then
       isabs = .false.
     else
       isabs = path(1:1) == sep
-  
+
       if ( .not. isabs) isabs = path(2:3) == ':/' .or. path(2:3) == ':\'
     endif
-  
+
   end function isabs
+
+
+  function relpath(path,start)
+
+    character(len=*), intent(in)           :: path
+    character(len=*), intent(in), optional :: start
+    character(len=:), allocatable          :: relpath
+
+    character(len=:), allocatable :: common_, path_, start_
+    integer :: l_common, l_path, i, j
+
+    if(isabs(path)) then
+      path_ = normpath(path)
+    else
+      path_ = normpath(join(getcwd(),path))
+    endif
+    l_path = len(path_)
+
+    if(.not. present(start)) then
+      start_ = getcwd()
+    else
+      if(isabs(start)) then
+        start_ = normpath(start)
+      else
+        start_ = normpath(join(getcwd(),start))
+      endif
+    endif
+
+    common_ = commonpath(start_,path_)
+    l_common = len(common_)
+
+    if(l_path == l_common) then
+      relpath = ''
+    else
+      if(common_ == sep) then
+        relpath = path_(l_common+1:)
+      else
+        relpath = path_(l_common+2:)
+      endif
+    endif
+
+    if(start_ /= common_) then
+      j = 0
+      l_common = max(1,l_common)
+
+
+      if(start_(2:3) == ':/' .or. start_(2:3) == ':\' ) then
+        j = 1
+        l_common = 4
+      endif
+
+      do i = l_common, len(start_(l_common:))
+        if(start_(i:i) == sep) j = j+1
+      enddo
+      relpath = repeat(curdir//curdir//sep,j)//relpath
+    endif
+
+    relpath = normpath(relpath)
+
+  end function relpath
 
 end submodule os_path
--- src/os_path.f90
+++ src/os_path.f90
@@ -51,12 +51,18 @@ module os_path
   end interface join
 
   interface
- 
+
     module pure function isabs(path)
       logical                      :: isabs
       character(len=*), intent(in) :: path
     end function isabs
-  
+
+    module function relpath(path,start)
+      character(len=*), intent(in)           :: path
+      character(len=*), intent(in), optional :: start
+      character(len=:), allocatable          :: relpath
+    end function relpath
+
   end interface
 
 
@@ -68,6 +74,8 @@ module os_path
     character(len=*), intent(in)  :: path
 
     abspath = normpath(join(getcwd(),path))
+
+
 
   end function abspath
 
@@ -466,68 +474,6 @@ module os_path
   end function normpath
 
   ! realpath
-
-  function relpath(path,start)
-
-    character(len=*), intent(in)           :: path
-    character(len=*), intent(in), optional :: start
-    character(len=:), allocatable          :: relpath
-
-    character(len=:), allocatable :: common_, path_, start_
-    integer :: l_common, l_path, i, j
-
-    if(isabs(path)) then
-      path_ = normpath(path)
-    else
-      path_ = normpath(join(getcwd(),path))
-    endif
-    l_path = len(path_)
-
-    if(.not. present(start)) then
-      start_ = getcwd()
-    else
-      if(isabs(start)) then
-        start_ = normpath(start)
-      else
-        start_ = normpath(join(getcwd(),start))
-      endif
-    endif
-
-    common_ = commonpath(start_,path_)
-    l_common = len(common_)
-
-    if(l_path == l_common) then
-      relpath = ''
-    else
-      if(common_ == sep) then
-        relpath = path_(l_common+1:)
-      else
-        relpath = path_(l_common+2:)
-      endif
-    endif
-
-    if(start_ /= common_) then
-      j = 0
-      l_common = max(1,l_common)
-
-      ! Take care of the drive indicator on Windows
-      if ( name == 'Windows' ) then
-        if(start_(2:3) == ':/' .or. start_(2:3) == ':\' ) then
-          j = 1
-          l_common = 4
-        endif
-      endif
-
-      do i = l_common, len(start_(l_common:))
-        if(start_(i:i) == sep) j = j+1
-      enddo
-      relpath = repeat(curdir//curdir//sep,j)//relpath
-    endif
-
-    relpath = normpath(relpath)
-
-  end function relpath
-
 
   function samefile(path1,path2)
 
