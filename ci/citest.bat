7750293c3d5678f615f559334c239f559c384f38
Ondřej Čertík <ondrej@certik.us>
2020-11-17 16:31:26 -0700

    Exit on error, reformat

--- ci/citest.bat
+++ ci/citest.bat
@@ -1,3 +1,10 @@
-cmake -DCMAKE_INSTALL_PREFIX=%cd%\inst .
+cmake ^
+    -DCMAKE_INSTALL_PREFIX=%cd%\inst ^
+    .
+if errorlevel 1 exit 1
+
 cmake --build . --config Release
+if errorlevel 1 exit 1
+
 .\src\stdlib_test
+if errorlevel 1 exit 1
--- ci/citest.sh
+++ ci/citest.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
-cmake -DCMAKE_INSTALL_PREFIX=`pwd`/inst .
+cmake \
+    -DCMAKE_INSTALL_PREFIX=`pwd`/inst \
+    .
+
 cmake --build . --config Release
+
 ./src/stdlib_test
