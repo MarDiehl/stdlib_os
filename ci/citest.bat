047b473ee94930fdc222ba8146b62d068968ffa1
Ondřej Čertík <ondrej@certik.us>
2020-11-17 16:59:03 -0700

    Win: Set -G and CMAKE_SH

--- ci/citest.bat
+++ ci/citest.bat
@@ -1,7 +1,9 @@
 cmake ^
-    -DCMAKE_Fortran_COMPILER=gfortran ^
+    -G "MinGW Makefiles" ^
+    -DCMAKE_SH="CMAKE_SH-NOTFOUND" ^
     -DCMAKE_INSTALL_PREFIX=%cd%\inst ^
     .
+
 if errorlevel 1 exit 1
 
 cmake --build . --config Release
